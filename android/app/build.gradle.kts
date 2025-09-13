a965d903b0f2f62fe8ce27b6dff0ac21ea502258
Schenier Lopez <schnierlopez@gmail.com>
2025-09-13 12:05:24 +0200

    fix ndkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.teslo_app"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
